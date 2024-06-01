c297e866d1036760374ac3c1f2d59800437e68e3
eliasvas <vaselas99@gmail.com>
2024-06-01 08:14:24 +0300

    Data structures!

--- gui/include/gui.h
+++ gui/include/gui.h
@@ -338,6 +338,7 @@ static ArenaTemp arena_get_scratch(Arena *conflict) {
 	return res;
 }
 
+static void arena_test() { printf("------Arena test!-----\n"); printf("---------------------\n"); ArenaTemp temp = arena_get_scratch(NULL); Arena *arena = arena_alloc(); u8 arr[5560]; u8 *mem = arena_push_nz(arena, kilobytes(1)); memcpy(mem, arr, 2560); mem = arena_push(arena, gigabytes(0.1)); printf("arena_current_pos=[%llud]", arena_current_pos(arena)); ArenaTemp t = arena_begin_temp(arena); void *large_mem = arena_push(arena, gigabytes(1)); printf("\nafter [10GB] arena_current_pos=[%llud]", arena_current_pos(arena)); printf("\nafter [10GB] temp_arena_current_pos=[%llud]", arena_current_pos(t.arena)); arena_end_temp(&t); printf("\nafter [POP] arena_current_pos=[%llud]\n", arena_current_pos(arena)); for (int i = 0; i < 9; ++i) { mem[i] = '0'+(9 - i); } printf("%s\n", &mem[0]); arena_release(arena); arena_end_temp(&temp); printf("---------------------\n"); }
 /*
 // WHY we need to pass conflict arena in arena_get_scratch(..);
 void* bar(Arena *arena){
@@ -365,8 +366,34 @@ void foo(){
 
     arena_end_temp(&temp);
 }
-
 */
+
+//-----------------------------------------------------------------------------
+// DATA STRUCTURES (mainly for use in conj. with Arena) 
+//-----------------------------------------------------------------------------
+
+#define sll_stack_push_N(f,n,next) ((n)->next=(f), (f)=(n))
+#define sll_stack_pop_N(f,next) ((f==0)?((f)=(f)):((f)=(f)->next))
+#define sll_stack_push(f,n) sll_stack_push_N(f,n,next)
+#define sll_stack_pop(f) sll_stack_pop_N(f,next)
+
+#define sll_queue_push_N(f,l,n,next) (((f)==0)?((f)=(l)=(n)):((l)->next=(n),(l)=(n),(n)->next=0))
+#define sll_queue_pop_N(f,l,next) (((f)==(l))?((f)=(l)=0):((f)=(f)->next))
+#define sll_queue_push(f,l,n) sll_queue_push_N(f,l,n,next)
+#define sll_queue_pop(f,l) sll_queue_pop_N(f,l,next)
+
+#define dll_push_back_NP(f,l,n,next,prev) (((f)==0)?((f)=(l)=(n),(n)->next=(n)->prev=0):((n)->prev=(l),(l)->next=(n),(l)=(n),((n)->next=0)))
+#define dll_push_back(f,l,n) dll_push_back_NP(f,l,n,next,prev)
+#define dll_push_front(f,l,n) dll_push_back_NP(l,f,n,prev,next)
+#define dll_remove_NP(f,l,n,next,prev) (((f)==(n))?((f)=(f)->next,(f)->prev=0):(((l)==(n))?((l)=(l)->prev,(l)->next=0):((n)->next->prev=(n)->prev,(n)->prev->next=(n)->next)))
+#define dll_remove(f,l,n) dll_remove_NP(f,l,n,next,prev)
+
+typedef struct TestNode TestNode;
+struct TestNode {TestNode*next;TestNode*prev;int data;};
+static void print_ll(TestNode *firstn) {printf("{");for(TestNode*n=firstn;n!=NULL;n=n->next){printf("%i,",n->data);}printf("}\n");}
+static void ll_test() { ArenaTemp temp = arena_get_scratch(NULL); printf("--Linked-List test!--\n"); printf("---------------------\n"); TestNode *ll_first = NULL; printf("SLL_STACK_PUSH: "); for (int i = 0; i < 10; ++i) { TestNode *n = push_array(temp.arena, TestNode, 1); n->data = i; sll_stack_push(ll_first, n); } print_ll(ll_first); printf("SLL_STACK_POP3E_I7:  "); sll_stack_pop(ll_first); sll_stack_pop(ll_first); sll_stack_pop(ll_first); TestNode *g0 = push_array(temp.arena, TestNode, 1); g0->data = 7; sll_stack_push(ll_first,g0); print_ll(ll_first); ll_first = NULL; TestNode *ll_last = NULL; printf("SLL_QUEUE_PUSH: "); for (int i = 0; i < 10; ++i) { TestNode *n = push_array(temp.arena, TestNode, 1); n->data = i; sll_queue_push(ll_first,ll_last, n); } print_ll(ll_first); printf("SLL_QUEUE_POP3E_I7:  "); sll_queue_pop(ll_first,ll_last); sll_queue_pop(ll_first,ll_last); sll_queue_pop(ll_first,ll_last); TestNode *g1 = push_array(temp.arena, TestNode, 1); g1->data = 7; sll_queue_push(ll_first,ll_last, g1); print_ll(ll_first); ll_first = NULL; ll_last = NULL; printf("DLL_PUSH_BACK:  "); for (int i = 0; i < 10; ++i) { TestNode *n = push_array(temp.arena, TestNode, 1); n->data = i; dll_push_back(ll_first,ll_last, n); } print_ll(ll_first); printf("DLL_REMOVE_ODD: "); for (TestNode *n = ll_first;n != NULL;n=n->next){ if (n->data % 2 == 0) { dll_remove(ll_first,ll_last,n); } } print_ll(ll_first); ll_first = NULL; ll_last = NULL; printf("DLL_PUSH_FRONT: "); for (int i = 0; i < 10; ++i) { TestNode *n = push_array(temp.arena, TestNode, 1); n->data = i; dll_push_front(ll_first,ll_last, n); } print_ll(ll_first); printf("DLL_REMOVE_ODD: "); for (TestNode *n = ll_first;n != NULL;n=n->next){ if (n->data % 2 == 0) { dll_remove(ll_first,ll_last,n); } } print_ll(ll_first); printf("---------------------\n"); arena_end_temp(&temp); }
+
+
 
 //-----------------------------------------------------------------------------
 // GENERIC HASH FUNCTION
--- sample/sample.c
+++ sample/sample.c
@@ -30,38 +30,10 @@ void sample_push_input_event(guiInputEvent e) {
     gui_input_push_event(&my_gui, e);
 }
 
-void hello_arena() {
-    printf("Hello Arena!\n");
-    ArenaTemp temp = arena_get_scratch(NULL);
-
-    Arena *arena = arena_alloc();
-    u8 arr[5560];
-    //void* other_mem = push_array(arena, u8, 256);
-   
-    u8 *mem = arena_push_nz(arena, kilobytes(1));
-    memcpy(mem, arr, 2560);
-    mem = arena_push_nz(arena, gigabytes(0.9));
-    printf("arena_current_pos=[%llud]", arena_current_pos(arena));
-    ArenaTemp t = arena_begin_temp(arena);
-    void *large_mem = arena_push_nz(arena, gigabytes(10));
-    printf("\nafter [10GB] arena_current_pos=[%llud]", arena_current_pos(arena));
-    printf("\nafter [10GB] temp_arena_current_pos=[%llud]", arena_current_pos(t.arena));
-    arena_end_temp(&t);
-    printf("\nafter [POP] arena_current_pos=[%llud]", arena_current_pos(arena));
-    for (int i = 0; i < 9; ++i) {
-        mem[i] = '0'+(9 - i);
-    }
-    printf("%s\n", &mem[0]);
-    // mem = arena_push(arena, gigabytes(1));
-    // mem = arena_push_nz(arena, gigabytes(5));
-    arena_release(arena);
-    arena_end_temp(&temp);
-    printf("Bye Arena!\n");
-}
-
 int main(){
     sample_init();
-    hello_arena();
+    arena_test();
+    ll_test();
     platform_init(my_gui.atlas.tex.data);
     for (;;){
         sample_update();
