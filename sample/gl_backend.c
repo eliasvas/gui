a47724ee24e61b86d98881d09d781a9a0279a769
eliasvas <vaselas99@gmail.com>
2024-06-29 18:39:17 +0300

    tweaks

--- sample/gl_backend.c
+++ sample/gl_backend.c
@@ -1,36 +1,267 @@
 #include "gui.h"
 #define SDL_MAIN_NOIMPL
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_opengles2.h>
+//#include <SDL3/SDL_opengles2.h>
 #include <SDL3/SDL_main.h>
+#include <GL/glew.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 // globals
+GLuint vao,vbo,sp;
 SDL_Window *window;
 SDL_GLContext glcontext;
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0) in vec2 inPos0;\n"
+"layout(location = 1) in vec2 inPos1;\n"
+"layout(location = 2) in vec2 inUV0;\n"
+"layout(location = 3) in vec2 inUV1;\n"
+"layout(location = 4) in vec4 inColor;\n"
+"layout(location = 5) in float inCornerRadius;\n"
+"layout(location = 6) in float inEdgeSoftness;\n"
+"layout(location = 7) in float inBorderThickness;\n"
+"\n"
+"uniform vec2 winDim;\n"
+"\n"
+"out vec2 fragUV;\n"
+"out vec2 fragDstPos;\n"
+"out vec2 fragDstCenter;\n"
+"out vec2 fragHalfSize;\n"
+"out vec4 fragColor;\n"
+"out float fragCornerRadius;\n"
+"out float fragEdgeSoftness;\n"
+"out float fragBorderThickness;\n"
+"\n"
+"const vec2 vertices[4] = vec2[4](\n"
+"    vec2(-1, -1),\n"
+"    vec2(-1, +1),\n"
+"    vec2(+1, -1),\n"
+"    vec2(+1, +1)\n"
+");\n"
+"\n"
+"void main() {\n"
+"    vec2 dstHalfSize = (inPos1 - inPos0) / 2.0;\n"
+"    vec2 dstCenter = (inPos1 + inPos0) / 2.0;\n"
+"    vec2 dstPos = vertices[gl_VertexID] * dstHalfSize + dstCenter;\n"
+"\n"
+"    gl_Position = vec4(2.0 * dstPos / winDim - 1.0, 0.0, 1.0);\n"
+"    gl_Position.y *= -1.0;\n"
+"\n"
+"    vec2 uvHalfSize = (inUV1 - inUV0) / 2.0;\n"
+"    vec2 uvCenter = (inUV1 + inUV0) / 2.0;\n"
+"    vec2 uvPos = vertices[gl_VertexID] * uvHalfSize + uvCenter;\n"
+"\n"
+"    fragUV = uvPos;\n"
+"    fragColor = inColor;\n"
+"    fragCornerRadius = inCornerRadius;\n"
+"    fragEdgeSoftness = inEdgeSoftness;\n"
+"    fragBorderThickness = inBorderThickness;\n"
+"    fragDstPos = dstPos;\n"
+"    fragDstCenter = dstCenter;\n"
+"    fragHalfSize = dstHalfSize;\n"
+"}\n";
 
-int SDL_Fail(){ SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError()); return -1; }
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"\n"
+"in vec2 fragUV;\n"
+"in vec2 fragDstPos;\n"
+"in vec2 fragDstCenter;\n"
+"in vec2 fragHalfSize;\n"
+"in vec4 fragColor;\n"
+"in float fragCornerRadius;\n"
+"in float fragEdgeSoftness;\n"
+"in float fragBorderThickness;\n"
+"\n"
+"out vec4 outColor;\n"
+"\n"
+"uniform sampler2D texture0;\n"
+"\n"
+"float roundedRectSDF(vec2 samplePos, vec2 rectCenter, vec2 rectHalfSize, float r) {\n"
+"    vec2 d2 = abs(rectCenter - samplePos) - rectHalfSize + vec2(r, r);\n"
+"    return min(max(d2.x, d2.y), 0.0) + length(max(d2, 0.0)) - r;\n"
+"}\n"
+"\n"
+"void main() {\n"
+"    ivec2 texSize = textureSize(texture0, 0);\n"
+"    float col = texture(texture0, fragUV / vec2(texSize)).r;\n"
+"    vec4 tex = vec4(col, col, col, col);\n"
+"\n"
+"    float softness = fragEdgeSoftness + 0.001;\n"
+"    float cornerRadius = fragCornerRadius;\n"
+"    vec2 softnessPadding = vec2(max(0, softness * 2.0 - 1.0), max(0, softness * 2.0 - 1.0));\n"
+"\n"
+"    float dist = roundedRectSDF(fragDstPos, fragDstCenter, fragHalfSize - softnessPadding, cornerRadius);\n"
+"    float sdfFactor = 1.0 - smoothstep(0.0, 2.0 * softness, dist);\n"
+"\n"
+"    float borderFactor = 1.0;\n"
+"    if (fragBorderThickness != 0.0) {\n"
+"        vec2 interiorHalfSize = fragHalfSize - vec2(fragBorderThickness);\n"
+"        float interiorRadiusReduceF = min(interiorHalfSize.x / fragHalfSize.x, interiorHalfSize.y / fragHalfSize.y);\n"
+"        float interiorCornerRadius = fragCornerRadius * interiorRadiusReduceF * interiorRadiusReduceF;\n"
+"\n"
+"        float insideD = roundedRectSDF(fragDstPos, fragDstCenter, interiorHalfSize - softnessPadding, interiorCornerRadius);\n"
+"        float insideF = smoothstep(0.0, 2.0 * softness, insideD);\n"
+"        borderFactor = insideF;\n"
+"    }\n"
+"\n"
+"    outColor = fragColor * tex * sdfFactor * borderFactor;\n"
+"}\n";
+
+GLuint compile_shader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+
+    GLint isCompiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE) {
+        GLint maxLength = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+        char errorLog[256];
+        glGetShaderInfoLog(shader, maxLength, &maxLength, errorLog);
+
+        SDL_Log("Shader compilation error: %s",errorLog);
+
+        glDeleteShader(shader);
+        return 0;
+    }
+    return shader;
+}
+
+GLuint create_sp(const char* vertexShaderSource, const char* fragmentShaderSource) {
+    GLuint vertexShader = compile_shader(GL_VERTEX_SHADER, vertexShaderSource);
+    if (vertexShader == 0) {
+        return 0;
+    }
+
+    GLuint fragmentShader = compile_shader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+    if (fragmentShader == 0) {
+        glDeleteShader(vertexShader);
+        return 0;
+    }
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    GLint isLinked = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, (int *)&isLinked);
+    if (isLinked == GL_FALSE) {
+        GLint maxLength = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+        char errorLog[256];
+        glGetProgramInfoLog(program, maxLength, &maxLength, errorLog);
+        SDL_Log("Shader compilation error: %s",errorLog);
 
 
+
+        glDeleteProgram(program);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        return 0;
+    }
+
+    glDetachShader(program, vertexShader);
+    glDetachShader(program, fragmentShader);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+#include <stddef.h> // for offsetof
+
+typedef struct InstanceData {
+    float pos0[2];
+    float pos1[2];
+    float uv0[2];
+    float uv1[2];
+    float color[4];
+    float corner_radius;
+    float edge_softness;
+    float border_thickness;
+}InstanceData;
+
+void fill_instance_vbo(GLuint instanceVBO, const InstanceData* instances, GLsizei instanceCount) {
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, instanceCount * sizeof(InstanceData), instances, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+GLuint create_instance_vbo(const InstanceData* instances, GLsizei instanceCount) {
+    GLuint vbo;
+    glGenBuffers(1, &vbo);  // Generate VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);  // Bind VBO
+
+    // Fill VBO with instance data
+    glBufferData(GL_ARRAY_BUFFER, instanceCount * sizeof(InstanceData), instances, GL_STATIC_DRAW);
+
+    // Unbind VBO to clean state
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    return vbo;  // Return the VBO ID
+}
+
+GLuint setup_instance_vao(GLuint vbo) {
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glEnableVertexAttribArray(0); // inPos0
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, pos0));
+
+    glEnableVertexAttribArray(1); // inPos1
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, pos1));
+
+    glEnableVertexAttribArray(2); // inUV0
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, uv0));
+
+    glEnableVertexAttribArray(3); // inUV1
+    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, uv1));
+
+    glEnableVertexAttribArray(4); // inColor
+    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, color));
+
+    glEnableVertexAttribArray(5); // inCornerRadius
+    glVertexAttribPointer(5, 1, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, corner_radius));
+
+    glEnableVertexAttribArray(6); // inEdgeSoftness
+    glVertexAttribPointer(6, 1, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, edge_softness));
+
+    glEnableVertexAttribArray(7); // inBorderThickness
+    glVertexAttribPointer(7, 1, GL_FLOAT, GL_FALSE, sizeof(InstanceData), (void*)offsetof(InstanceData, border_thickness));
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    return vao;
+}
 
 void platform_init(u8 *font_atlas_data) {
     // init the library, here we make a window so we only need the Video capabilities.
-    if (SDL_Init(SDL_INIT_VIDEO))
-    {
-        return;
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        exit(1);
     }
     // create a window
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     window = SDL_CreateWindow("sample", 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
     glcontext = SDL_GL_CreateContext(window);
-    if (!glcontext)
-    {
-        return;
+    if (!glcontext) {
+        exit(1);
     }
+    SDL_GL_MakeCurrent(window,glcontext);
+    glewInit();
 
-    // print some information about the window
     SDL_ShowWindow(window);
     {
         int width, height, bbwidth, bbheight;
@@ -43,6 +274,13 @@ void platform_init(u8 *font_atlas_data) {
         }
     }
     SDL_Log("Application started successfully!");
+    vbo = create_instance_vbo(NULL, 0);
+    fill_instance_vbo(vbo, NULL, 0);
+    SDL_Log("vbo OK!");
+    vao = setup_instance_vao(vbo);
+    SDL_Log("vao OK!");
+    sp = create_sp(vertexShaderSource, fragmentShaderSource);
+    SDL_Log("Shader program OK!");
 }
 
 void platform_update() {
@@ -67,6 +305,12 @@ void platform_render(guiRenderCommand *rcommands, u32 command_count)
     glClearColor(red, green, blue, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
     // Here we draw
+    fill_instance_vbo(vbo, (InstanceData*)rcommands, command_count);
+    glUseProgram(sp);
+    glBindVertexArray(vao);
+    glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, command_count);
+    glBindVertexArray(0);
+    //---------
     SDL_GL_SwapWindow(window);
 }
 
